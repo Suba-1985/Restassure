e7ed8fc374b74382766523e048cc461de4c0f2cc
suba <suba>
2023-07-20 18:47:00 -0400

    second commit suba

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,5 +1,23 @@
 package runner;
 
+import org.testng.annotations.DataProvider;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/featuresFiles",
+        glue = {"stepDefinition"}
+)
 public class TestRunner {
 
 }
+
+		
+		
+
+
+
+
